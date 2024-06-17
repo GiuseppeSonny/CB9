2512c6091dcc4dc68598c7ee6a686b706221ed22
GiuseppeSonny <umanella23@gmail.com>
2024-06-17 16:40:20 +0200

    footer Add

--- esercitazione-instaReact/src/components/Footer/Footer.jsx
+++ esercitazione-instaReact/src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import styles from "./footer.module.scss"; 
 import { GrHomeRounded } from "react-icons/gr";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaRegPlusSquare } from "react-icons/fa";
+import { BiMoviePlay } from "react-icons/bi";
 
 const Footer = () => {
     const handleButtonClick = () => {
@@ -8,7 +11,9 @@ const Footer = () => {
     return (
         <div className={styles.footerContainer}>
             <GrHomeRounded className={styles.iconHome} onClick={handleButtonClick}/>
-
+            <IoSearchSharp className={styles.iconSearch} onClick={handleButtonClick}/>
+            <FaRegPlusSquare className={styles.iconPlus} onClick={handleButtonClick}/>
+            <BiMoviePlay className={styles.iconPlay} onClick={handleButtonClick}/>
         </div>
     )
 }
