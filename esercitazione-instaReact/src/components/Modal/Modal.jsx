04278bd9a49ddbb479cf12a60ef9f9afb30e181e
GiuseppeSonny <umanella23@gmail.com>
2024-06-22 12:47:34 +0200

    home btn

--- esercitazione-instaReact/src/components/Modal/Modal.jsx
+++ esercitazione-instaReact/src/components/Modal/Modal.jsx
@@ -1,12 +1,20 @@
 import PostCarousel from "../PostCarousel/PostCarousel";
 import styles from "./modal.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const Modal = ({ handleCloseClick }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
     <div className={styles.modalContainer}>
       <PostCarousel className={styles.modalCarousel} />
       <button onClick={handleCloseClick} className={styles.closeButton}>
         X
+      </button>
+      <button onClick={handleClick} className={styles.homeButton}>
+        Home
       </button>
     </div>
   );
